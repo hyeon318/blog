aca5192095c7e0ced7a9b494805e83011747522f
spidey <kyurin123@gmail.com>
2023-07-24 22:22:55 +0900

    tailwind 속성 적용

--- src/components/FeaturedPosts.tsx
+++ src/components/FeaturedPosts.tsx
@@ -1,14 +1,14 @@
-import { getAllPosts } from '@/service/posts';
+import { getFeaturedPosts } from '@/service/posts';
 import PostsGrid from './PostsGrid';
 
 export default async function FeaturedPosts() {
   // 1. post list
   // 2. all post data
 
-  const posts = await getAllPosts();
+  const posts = await getFeaturedPosts().then(data => data);
 
   return  <section>
-            <h2>Fetured Posts</h2>
+            <h2 className='text-2xl font-bold my-2'>Fetured Posts</h2>
             <PostsGrid posts={posts}/>
           </section>;
 }
